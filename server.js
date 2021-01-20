641a61922a51d08e52a4cb71000521037c33b73d
Ramazan774 <ramazan.ramazanov@glion.ch>
2021-01-19 23:39:05 -0800

    backend api

--- server.js
+++ server.js
@@ -53,6 +53,36 @@ app.post("/register", (req, res) => {
   });
   res.json(database.users[database.users.length - 1]);
 });
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((users) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.boidy;
+  let found = false;
+  database.users.forEach((users) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("hi");
 });
