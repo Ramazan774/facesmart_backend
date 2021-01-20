914fd2bca495aa62c586d06f8ebadb5cd443436e
Ramazan774 <ramazan.ramazanov@glion.ch>
2021-01-19 23:28:19 -0800

    signin register added

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('this is working');
-})
+app.use(bodyParser.json());
 
-app.listen(3000, ()=> {
-    console.log('hi')
-})
+const database = {
+  user: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "john123",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "sally123",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error loggin in");
+  }
+  res.json("signing");
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+app.listen(3000, () => {
+  console.log("hi");
+});
